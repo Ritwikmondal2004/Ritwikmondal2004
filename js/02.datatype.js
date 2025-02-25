47687da1792ba27cdcd277d51d35162504246c73
Ritwikmondal2004 <118586439+Ritwikmondal2004@users.noreply.github.com>
2025-02-25 02:44:59 +0000

    datatypes

---
+++ js/02.datatype.js
@@ -0,0 +1,22 @@
+"use strict" // treat all js code as newer version
+//alert(3+3) // we are using nodejs not browser
+
+console.log(3+3)
+
+
+//number limit => 2 to power 53;
+//bigint
+//string=>""
+//boolean=>true/false;
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+
+//object
+let string = "hitesh"
+console.log(string)
+console.log(typeof string)
+
+console.log(typeof undefined);// undefined
+console.log(typeof null); // object
