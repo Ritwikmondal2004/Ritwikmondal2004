0fbb287788cfa6283618eb6a72d1afdeb4385127
Ritwikmondal2004 <118586439+Ritwikmondal2004@users.noreply.github.com>
2025-02-25 01:37:30 +0000

    comparison

---
+++ js/comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+    // avoide this type of convertion which may you confused; we are not use this convertion
+// === 
+
+console.log("2" === 2);
