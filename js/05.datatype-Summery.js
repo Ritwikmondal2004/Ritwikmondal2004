3630a2835b7aa3db616dba5dfc87f57259a43857
Ritwikmondal2004 <118586439+Ritwikmondal2004@users.noreply.github.com>
2025-02-25 02:47:54 +0000

    data-Type

---
+++ js/05.datatype-Summery.js
@@ -0,0 +1,20 @@
+//primitive
+
+
+// string ,number,boolearn,null,undifiend,symbol,BigInt
+
+const score=100;
+const scoreValue=100.3;
+const isLoggedIn=false;
+const outsideTemp=null;
+let userEmail;
+const id = Symbol('123')
+const anotherId=Symbol('123')
+
+ console.log(id==anotherId);  //return value same noyy
+
+console.log(scoreValue);
+
+
+//refurence(Non premative datatype)
+//array,objects,function
