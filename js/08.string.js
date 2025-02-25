3e4063036c4ac4a572f7d747934f14bfac9fd019
Ritwikmondal2004 <118586439+Ritwikmondal2004@users.noreply.github.com>
2025-02-25 18:14:59 +0000

    string

--- js/08.string.js
+++ js/08.string.js
@@ -5,6 +5,28 @@ let age = 22;
 console.log(`hello my name is ${name} and my age is ${age}`);
 
 
-const stringName = new String('ritwik');
+const gameName = new String('ritwik');
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
 
-console.log(stringName.__proto__);
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
