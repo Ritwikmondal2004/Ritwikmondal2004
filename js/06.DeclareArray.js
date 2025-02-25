b85d4f1fcf8ce0d39562964a3140f3ae93e52758
Ritwikmondal2004 <118586439+Ritwikmondal2004@users.noreply.github.com>
2025-02-25 02:49:10 +0000

    DeclareArray

---
+++ js/06.DeclareArray.js
@@ -0,0 +1,11 @@
+const heros = ["saktiman","nagraj","doga"] // define this array;
+let myobj={
+    name:"ritwik",
+    age: 22,
+}
+console.log(myobj);
+
+// declare function
+const Funtion = function(){
+    console.log("hello ")
+}
