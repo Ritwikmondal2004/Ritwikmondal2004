359e13dedd232b5b08095523ad7c7ec697c920f3
Ritwikmondal2004 <118586439+Ritwikmondal2004@users.noreply.github.com>
2025-02-25 04:27:47 +0000

    stack-heap

---
+++ js/07.heap_Stack.js
@@ -0,0 +1,21 @@
+// satck (Primitive), Heap (Non - Primitive)
+
+let myYoutubeName = "Ritwik Kumar Mondal"
+
+let anotherName = myYoutubeChannel
+anotherName = "chaiAurCode"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "ritwik@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
