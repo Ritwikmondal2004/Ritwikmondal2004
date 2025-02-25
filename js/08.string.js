42125ee8566efba1255a74cba6b7243fd0621f5c
Ritwikmondal2004 <118586439+Ritwikmondal2004@users.noreply.github.com>
2025-02-25 05:31:58 +0000

    string

---
+++ js/08.string.js
@@ -0,0 +1,10 @@
+let name = "ritwik";
+let age = 22;
+
+// this is the original string structure which any one write this formate;
+console.log(`hello my name is ${name} and my age is ${age}`);
+
+
+const stringName = new String('ritwik');
+
+console.log(stringName.__proto__);
