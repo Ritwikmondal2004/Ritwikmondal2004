02a1bdb48bbece2a64aa1fb467ea4a1270936f42
Ritwikmondal2004 <118586439+Ritwikmondal2004@users.noreply.github.com>
2025-03-03 08:42:54 +0000

    date_and_time

---
+++ js/10.Date_and_time.js
@@ -0,0 +1,21 @@
+// Dates
+
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);//******** */
+
+let mycreatedate=new Date("01-14-2023"); // month start with 0;
+console.log(mycreatedate.toDateString());
+console.log(mycreatedate.toLocaleString());
+
+
+
+//------------------time stamp----------------------
+
+  let myTimeStamp=Date.now();
+
+  console.log(myTimeStamp);
+  console.log(mycreatedate);
+  
