140886ab49a07a9559503f5363f00e330c441105
Ritwikmondal2004 <118586439+Ritwikmondal2004@users.noreply.github.com>
2025-03-27 04:57:46 +0000

    for loop

---
+++ js/loop/01.for_loop.js
@@ -0,0 +1,16 @@
+//for
+// const n=10
+// for(let i=0;i<n;i++){
+//     console.log(`outer loop: ${i}`);
+//     for(let j=0;j<n;j++){
+//         console.log(`neer loop: ${j}----${i}`)
+//     }
+// }
+
+
+//another loop
+let array=["ritwik","flash","dipak"]
+for( let i=0;i<array.length;i++){
+    
+    console.log(`${i}`,array[i])
+}
