f312f5c8703a1d6839ee96b1965594479b4f46ec
Ritwikmondal2004 <118586439+Ritwikmondal2004@users.noreply.github.com>
2025-03-27 09:40:07 +0000

    three

---
+++ js/loop/02.three.js
@@ -0,0 +1,29 @@
+const greet = "hello world"
+for(let i=0;i<=greet.length;i++){
+    console.log(`${i}--${greet}`)
+}
+
+//maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    //console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
