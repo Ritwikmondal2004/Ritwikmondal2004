f0c8b11f1b296e15367350e7a4ab023b62211336
Ritwikmondal2004 <118586439+Ritwikmondal2004@users.noreply.github.com>
2025-03-27 09:44:22 +0000

    three

--- js/loop/02.three.js
+++ js/loop/02.three.js
@@ -1,6 +1,17 @@
-const greet = "hello world"
-for(let i=0;i<=greet.length;i++){
-    console.log(`${i}--${greet}`)
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
 }
 
 //maps
@@ -12,15 +23,16 @@ map.set('Fr', "France")
 map.set('IN', "India")
 
 
-// console.log(map);
+ console.log(map);
 
 for (const [key, value] of map) {
-    //console.log(key, ':-', value);
+    console.log(key, ':-', value);
 }
 
 const myObject = {
     game1: 'NFS',
     game2: 'Spiderman'
+    
 }
 
 // for (const [key, value] of myObject) {
